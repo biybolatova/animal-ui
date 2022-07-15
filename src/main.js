e1d2d6d8f76b80436c4dee2d99464122c33e84be
Albina Biybolatova <user1@example.com>
2022-07-15 11:54:15 +0500

    UI | init project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "@/router"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import Toast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
+createApp(App)
+    .use(router)
+    .use(Toast)
+    .mount("#app")
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+
+]
+
+const index = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default index;
